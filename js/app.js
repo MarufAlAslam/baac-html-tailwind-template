7bea92e32352de53017704bb918fd0f6cef6c3cd
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-09-20 15:21:55 +0600

    sponsors done

--- js/app.js
+++ js/app.js
@@ -11,3 +11,37 @@ setInterval(() => {
     hero.classList.remove("fade-in");
   }, 5000);
 }, 5000);
+
+$(".slider").slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
