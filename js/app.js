82b7db690588e144997fb400fc7138d309d754ab
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-09-30 16:45:11 +0600

    updated slider and message from chairman

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const hero = document.getElementById("hero");
 
 // change hero background in every 5 seconds
 setInterval(() => {
-  const random = Math.floor(Math.random() * 3) + 1;
+  const random = Math.floor(Math.random() * 2) + 1;
   hero.style.backgroundImage = `linear-gradient(#003E80, rgba(0,0,0,0.5)), url(../img/hero${random}.jpg)`;
 
   //   add fade in animation
